5b1fca44f3c56bd06039a4a34942569a92e64d8e
jwatts41 <wattsjonovan@yahoo.com>
2022-11-06 17:52:32 -0500

    ran snapshot  tests for githubCard

---
+++ snapshot-tests/src/GitHubCard/GitHubCard.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Card from "react-bootstrap/Card"
+
+
+function GitHubCard() {
+  return (
+      <div className="card">
+        <Card style={{ width: "18rem"}}>
+        <Card.Body>
+            <Card.Title>Jwatts41</Card.Title>
+            <Card.Text>
+            I love coding and I am interested in learning as many coding languages as possible.
+            </Card.Text>
+        </Card.Body>
+        </Card>
+      </div>
+  );
+}
+
+export default GitHubCard
+
